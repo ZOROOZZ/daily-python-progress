720298c1820e145152398735d2ae74990a4d9b1c
Mehul Saini <mehulsaini675@gmail.com>
2026-04-16 21:35:52 +0530

    [Web] Update main.py (by admin)

--- Day_1/main.py
+++ Day_1/main.py
@@ -1,2 +1,5 @@
-number = input("Enter five different numbers: ")
-print(number)
+number = int(input("Enter five different numbers: "))
+if number % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
