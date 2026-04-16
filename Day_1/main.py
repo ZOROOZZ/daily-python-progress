15a6ef7430b4467ed61703752a2195c7243bbdba
Mehul Saini <mehulsaini675@gmail.com>
2026-04-16 21:48:32 +0530

    [Web] Update main.py (by admin)

--- Day_1/main.py
+++ Day_1/main.py
@@ -3,5 +3,5 @@ numbers = [12, 45, 34, 76, 15]
 for number in numbers:
     if number % 2 == 0:
         print(f"{number} is an Even Number")
-    else:
-        print(f"{number} is an Odd Number")
+ #   else:
+    #    print(f"{number} is an Odd Number")
