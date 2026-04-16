98d1c69eae929910d801901615e45be82c7ef09a
Mehul Saini <mehulsaini675@gmail.com>
2026-04-16 21:06:23 +0530

    [Web] Update statements.py (by admin)

--- Day_1/statements.py
+++ Day_1/statements.py
@@ -1,3 +1,9 @@
 print("Hello, World!")
 print("Have a good day.")
 print("Learning Python is fun!")
+
+#Semincolons
+print("Hello"); print("How are you ?"); print("Bye bye!")
+
+#Error
+#print("Python is Fun!") print("Really!")
