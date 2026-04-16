9abfeb412fee4f00e72e2f417ad0476692f03355
Mehul Saini <mehulsaini675@gmail.com>
2026-04-16 21:46:09 +0530

    [Web] Update main.py (by admin)

--- Day_1/main.py
+++ Day_1/main.py
@@ -1,5 +1,7 @@
-number = int(input("Enter five different numbers: "))
-if number % 2 == 0:
-    print("Even Number")
-else:
-    print("Odd Number")
+numbers = [12, 45, 34, 76, 15]
+
+for number in numbers:
+    if number % 2 == 0:
+        print(f"{number} is an Even Number")
+    else:
+        print(f"{number} is an Odd Number")
