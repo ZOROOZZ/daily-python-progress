de6b745ea2e22b2fdf444743e8e886464f3a8d0b
Mehul Saini <mehulsaini675@gmail.com>
2026-04-17 19:55:06 +0530

    [Web] Update def2.py (by admin)

---
+++ Day_2/def2.py
@@ -0,0 +1,18 @@
+even_numbers_only = []
+def even_list(numbers):
+    for number in numbers:
+        if number % 2 == 0:
+            even_numbers_only.append(number)
+
+first = [23, 45, 67, 34, 89]
+second = [8, 17, 26, 29, 37]
+even_list(first)
+
+even_list(second)
+
+print(even_numbers_only)
+        
+        
+        
+    
+    
