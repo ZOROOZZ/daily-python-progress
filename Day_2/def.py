eade6907c8c3f3d9161d8b8f38f9e6c543b5b8cd
Mehul Saini <mehulsaini675@gmail.com>
2026-04-17 11:35:58 +0530

    [Web] Update def.py (by admin)

--- Day_2/def.py
+++ Day_2/def.py
@@ -1,12 +1,16 @@
 def even_number_list(numberlist):
     for number in numberlist:
         if number % 2 == 0:
-            print("Even Number")
+            print(f"{number} is an Even Number")
 
 first_batch = [12, 35, 43, 56, 23]
 second_batch = [13, 36, 44, 57, 24]
 
+print("Checking First Batch")
+even_number_list(first_batch)
 
+print("Checking Second Batch")
+even_number_list(second_batch)
             
         
     
