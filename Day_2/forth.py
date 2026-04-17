5b33f1f653110767b6e3f49917b2881123281b58
Mehul Saini <mehulsaini675@gmail.com>
2026-04-17 21:42:46 +0530

    [Web] Update forth.py (by admin)

--- Day_2/forth.py
+++ Day_2/forth.py
@@ -1,3 +1,14 @@
+market_data = [
+    {"ticker": "AAPL", "price": 150, "trend": "up"},
+    {"ticker": "TSLA", "price": 200, "trend": "down"},
+    {"ticker": "GOOG", "price": 130, "trend": "up"}
+]
 def analyze_stocks(market_data):
     
-    
+    for Ticker in market_data:
+        if trend:up:
+            print(f"[Ticker] is going up!")
+        else:
+            print(f"[Ticker] is going down!")
+            
+            
